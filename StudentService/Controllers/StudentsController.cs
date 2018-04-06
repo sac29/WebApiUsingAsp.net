1d65f78b74adc86af42cc71cfad600dfa73a0924
sachin.vishwakarma <sachin.vishwakarma@DEL1-DHP-32427>
2018-04-06 16:23:19 +0530

    Add project files.

---
+++ StudentService/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StudentDataAccess;
+namespace StudentService.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        public IEnumerable<Student> Get()
+        {
+            using(StudentDBEntities entities = new StudentDBEntities())
+            {
+                return entities.Students.ToList();
+            }
+        }
+        public Student Get(int id)
+        {
+            using (StudentDBEntities entities = new StudentDBEntities())
+            {
+                return entities.Students.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
